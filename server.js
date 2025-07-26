f11f405d80da9273e9ed29338aec578534c72954
Ismael Pastor <ispaluan@gmail.com>
2025-07-26 10:06:31 +0200

    Seguir seguir

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const orderRoutes = require('./routes/orderRoutes')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://qr-orders-frontend.onrender.com'
+}));
 app.use(express.json())
 
 connectDB()
