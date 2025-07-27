d886f0b1e85e17fd30e3361dfd214c5cd7ce439e
Ismael Pastor <ispaluan@gmail.com>
2025-07-27 10:46:46 +0200

    mas cambios

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router();
 const protect = require('../controllers/menuController');
-const { createMenuItem } = require('../controllers/menuController'); 
 
 const {
     createMenuItem,
