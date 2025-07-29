714ed02c392fb7e247fd5ebabadf2315358c6823
Ismael Pastor <ispaluan@gmail.com>
2025-07-29 18:56:49 +0200

    we

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getRestaurant } = require('../controllers/restaurantController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 
 
-
-router.get('/', getRestaurant);
+router.get('/', authMiddleware , getRestaurant);
 
 module.exports = router;
